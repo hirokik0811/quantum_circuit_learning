568bdbec4f96014cf4970d6f0e89c6d91221d1a7
hirokik0811 <32285179+hirokik0811@users.noreply.github.com>
2019-05-09 05:55:18 -0400

    Parameters comparison

--- test.py
+++ test.py
@@ -9,57 +9,42 @@ from model import QCL
 from qulacs import Observable
 
 SAMPLE_SIZE = 256
-BATCH_SIZE = 10
+BATCH_SIZE = 1
 N_QUBITS = 6
 DEPTH = 6
 EPOCHS = 256
 GRADIENT_METHOD='steepest'
-ALPHA = 0.1
+ALPHA = 0.04
 
-loss_lists = []
-
-for batch_size in [1, 10, 100]:
-    X = np.random.uniform(-1.0, 1.0, (SAMPLE_SIZE, 1))
-    Y = X**2
-    Pauli_string = "Z 0"
-    observable = Observable(N_QUBITS)
-    observable.add_operator(1.0,Pauli_string)
+loss_lists = {}
+X_orig = np.random.uniform(-2.0, 2.0, (SAMPLE_SIZE, 1))
+Y = X_orig**2
+X = X_orig/2
+for batch_size in [1]:
+    for method in ['steepest']:#['steepest', 'quasi-newton']:
+        Pauli_string = "Z 0"
+        observable = Observable(N_QUBITS)
+        observable.add_operator(1.0,Pauli_string)
+        
+        qcl = QCL(N_QUBITS, DEPTH)
+        loss_lists['%s_bs_%d' % (method, batch_size)] = qcl.fit_regression(X, Y, observable, max_itr = EPOCHS, batch_size = batch_size, gradient_method=method, step_size = ALPHA)
     
-    qcl = QCL(N_QUBITS, DEPTH)
-    loss_list1 = qcl.fit_regression(X, Y, observable, max_itr = EPOCHS, batch_size = BATCH_SIZE, gradient_method=GRADIENT_METHOD, step_size = ALPHA)
-    
-    
-    pred = []
-    for x in X:
-        pred.append(qcl.get_expectation(x, qcl.theta, observable))
-    """
-    plt.scatter(X, pred)
-    plt.scatter(X, Y)
-    plt.xlabel("x")
-    plt.ylabel("f(x)=x^2")
-    plt.show()
-    """
-"""
-plt.plot(range(EPOCHS), loss_list1, label='steepest descent')
-plt.xlabel("epoch")
-plt.ylabel("loss")
-plt.show()
-"""
-
-
-qcl = QCL(N_QUBITS, DEPTH)
-loss_list2 = qcl.fit_regression(X, Y, observable, max_itr = EPOCHS, batch_size = BATCH_SIZE, gradient_method='quasi-newton', step_size = ALPHA)
-
-plt.plot(range(EPOCHS), loss_list1, label='steepest descent')
-plt.plot(range(EPOCHS), loss_list2, label='quasi-newton')
+        pred = []
+        for x in X:
+            pred.append(qcl.amp*qcl.get_expectation(x, qcl.theta, observable))
+        
+        plt.scatter(X, pred)
+        plt.scatter(X, Y)
+        plt.xlabel("x")
+        plt.ylabel("f(x)=x^2")
+        plt.savefig('./images/batchsize_%d_method_%s.png' % (batch_size, method))
+        plt.close()
+        
+for key, loss_list in loss_lists.items():
+    plt.plot(range(EPOCHS), loss_list, label=key)
 plt.xlabel("epoch")
 plt.ylabel("loss")
 plt.legend()
-plt.show()
+plt.savefig('./images/convergence.png')
+plt.close()
 
-pred = []
-for x in X:
-    pred.append(qcl.get_expectation(x, qcl.theta, observable))
-plt.scatter(X, pred)
-plt.scatter(X, Y)
-plt.show()
